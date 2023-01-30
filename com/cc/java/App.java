8f64d7432a0d996a3bb74d57074dcb656f9ef5b6
Peter <schmidt.multimedia@gmail.com>
2023-01-30 11:16:15 +0100

    tellYourAddress() / 1. Iteration

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,9 +4,16 @@ public class App {
     
     public static void main(String[] args) {
 
-        // 1. Instanziierung class --> object
+        // 1. Instanziierung class --> object / instance
         Cat cat = new Cat(); 
         System.out.println(cat);
+        cat.tellYourAddress();
+
+        System.out.println("-------------------------");
+
+        Cat cat2 = new Cat();
+        System.out.println(cat2);
+        cat2.tellYourAddress();
 
     }
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -3,7 +3,8 @@ package com.cc.java;
 public class Cat {
 
    
+    public void tellYourAddress() {
+        System.out.println(this);
+    }
     
-    
-
 }
