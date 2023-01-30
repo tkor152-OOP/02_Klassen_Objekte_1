440036ff978aba4c05b444531e65ad506ba6be1a
Peter <schmidt.multimedia@gmail.com>
2023-01-30 10:31:25 +0100

    // 1. Instanziierung class --> object

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,11 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
+        // 1. Instanziierung class --> object
+        Cat cat = new Cat(); 
+        System.out.println(cat);
+
     }
 
   
