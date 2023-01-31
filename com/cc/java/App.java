cab45c78093c1fae21214b13337403010ae2b0ea
Peter <schmidt.multimedia@gmail.com>
2023-01-31 10:18:45 +0100

    Felder & Methoden / 1. Iteration

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,12 +5,30 @@ public class App {
     public static void main(String[] args) {
 
         Cat cat = new Cat(); 
+
+        cat.name = "Grizabella";
+        cat.furColor = "white";
+        cat.age = 29;
+
+        output(cat.name);
+        output(cat.furColor);
+        output(String.valueOf(cat.age));  // Typkonvertierung
+
         output("Blick von aussen: " + cat);
         output(cat.tellYourAddress());
 
         output("-------------------------");
 
         Cat cat2 = new Cat();
+
+        cat2.name = "Alonzo";
+        cat2.furColor = "grey";
+        cat2.age = 35;
+
+        output(cat2.name);
+        output(cat2.furColor);
+        output(String.valueOf(cat2.age));  // Typkonvertierung
+
         output("Blick von aussen: " + cat2);
         output(cat2.tellYourAddress());
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,6 +2,9 @@ package com.cc.java;
 
 public class Cat {
 
+  public String name;
+  public String furColor;
+  public int age;
 
     public String tellYourAddress() {
       return  "Blick von innen: " + this;
